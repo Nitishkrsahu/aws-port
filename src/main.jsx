12411e7eb48bbffb9837ea3cc59cf11399a26897
nitish <nitishkrsahu123@gmail.com>
2026-04-24 14:55:40 +0530

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
 import App from './App.jsx'
 
